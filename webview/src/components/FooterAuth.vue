7da858516302cd0c00ec45ac14eea63ef0052a6f
kilkyungwan <forallabouttest@gmail.com>
2023-03-20 18:15:09 +0900

    auth 용 footer, top bar 추가

---
+++ webview/src/components/FooterAuth.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-footer fixed app>
+      <div>문의: example@gmail.com</div>
+    </v-footer>
+  </div>
+</template>
+<script>
+import { computed, ref } from "vue";
+import { useStore } from "vuex";
+export default {
+	methods: {
+  }
+
+}
+</script>
