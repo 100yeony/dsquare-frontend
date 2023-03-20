2e24d863f47d1ccdc86fa3b105fdc8a150931fff
kilkyungwan <forallabouttest@gmail.com>
2023-03-20 17:14:44 +0900

    home 참고 내용 추가

--- webview/src/pages/home/index.vue
+++ webview/src/pages/home/index.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    HOME
+    <!--
+    https://kin.naver.com/hall/index.naver?year=2023&month=2
+    https://m.kin.naver.com/mobile/hall/index.naver
+    -->
+    명예의 전당.
+    사용자랭킹
+    게시글목록
+
   </div>
 </template>
