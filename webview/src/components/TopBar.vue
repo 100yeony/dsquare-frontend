c8be4ecb37b07eb8c593a11ff347b0fbf09ff32e
kilkyungwan <forallabouttest@gmail.com>
2023-03-20 17:14:00 +0900

    top push notificattion 형식 추가(테스트), 검색(테스트) 추가

--- webview/src/components/TopBar.vue
+++ webview/src/components/TopBar.vue
@@ -4,7 +4,38 @@
       <v-btn icon size="x-large" @click="onclickBackBtn()" v-if="back">
         <v-icon>mdi-arrow-left</v-icon>
       </v-btn>
-      <v-toolbar-title>{{ menuTitle }}</v-toolbar-title></v-app-bar
+      <v-toolbar-title>{{ menuTitle }}</v-toolbar-title>
+      <!--
+      아래 내용들을 이용해서, 검색, push를 custom 진행하세요.
+       -->
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <!-- 아래는 테스트로, 만약 push notifications가 0보다 큰 경우네는 notification의 color가 primary로 하는 예시입니다. -->
+        <v-menu>
+          <template v-slot:activator="{ props }">
+            <v-btn icon
+              :color="notifications.length > 0 ? 'primary' : ''"
+              v-bind="props"
+            >
+              <v-badge :content="notifications.length">
+                <v-icon>mdi-bell</v-icon>
+              </v-badge>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item
+              v-for="(notification, index) in notifications"
+              :key="id"
+              :value="id"
+            >
+              <v-list-item-title>{{ notification.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+
+      </v-app-bar
     >
   </div>
 </template>
@@ -22,6 +53,14 @@ export default {
     const back = ref('');
     return { menuTitle, back };
   },
+  data: () => ({
+    notifications : [
+      { title: '테스트 push 01', id: 0 },
+      { title: '테스트 push 02', id: 1 },
+      { title: '테스트 push 03', id: 2 },
+      { title: '테스트 push 04', id :3 },
+    ],
+  }),
   watch:{
       $route (to, from){
         this.back = to.meta.back;
