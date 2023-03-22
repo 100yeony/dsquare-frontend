3e3286b799f722d061d23b221d9ebb009cc39210
kilkyungwan <forallabouttest@gmail.com>
2023-03-22 11:40:18 +0900

    자유게시판 detail 형식

---
+++ webview/src/pages/board/free/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <h4>자유게시판 상세</h4>
+  <h4>제목 : </h4>
+  <h6>번호 : {{qnaId}} </h6>
+  <body>내용 넣으세요</body>
+</template>
+<script>
+export default {
+    components: {
+    },
+    data() {
+      return {
+        freeId: '',
+      };
+    },
+  mounted(){
+    console.log(this.$route.query.id)
+    if(!this.$route.query.id) { // work 값이 없으면.
+      this.$router.replace(process.env.VUE_APP_BOARD)
+    }
+  },
+};
+</script>
