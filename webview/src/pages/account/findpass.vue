9888da8bbd6d8083c3461739d39421b9f9f449a1
우예원 <yeweon@uyewon-ui-MacBookPro.local>
2023-04-04 15:27:16 +0900

    feat: change findpass design

--- webview/src/pages/account/findpass.vue
+++ webview/src/pages/account/findpass.vue
@@ -57,35 +57,35 @@ export default {
 </script>
 
 <template>
-  <v-container class="ph-100 pw-100">
+  <v-container class="ph-55 pw-100">
     <v-row class="ph-100">
       <v-col class="pw-100" align-self="center">
         <form @submit.prevent="tryToReset">
-          <v-container class="pw-90">
-            <h2 class="text-primary text-center mb-5">비밀번호 변경</h2>
-            <div class="pph-10 text-center font-sm mb-5">
+          <v-container class="max-pw">
+            <h2 class="text-primary text-center mb-5">비밀번호 찾기</h2>
+            <div class="pph-10 text-center font-sm mb-7">
               회원 가입시 입력하신 이메일 주소를 입력하시면, 해당 이메일로 임시 비밀번호를 발급해드립니다.
             </div>
-            <v-card>
-              <v-col>
-                <v-text-field label="이메일을 입력해주세요" type="text" v-model="email" prepend-inner-icon="mdi-account"
-                  class="form-control font-sm" id="useremail" density="compact" :class="{
-                    'is-invalid': submitted && v$.email.$error,
-                  }" />
-                <div v-if="submitted && v$.email.$error" class="invalid-feedback">
-                  <span v-if="v$.email.required.$invalid" class="font-xs font_red">이메일을 입력해주세요.</span>
-                  <span v-if="v$.email.email.$invalid" class="font-xs font_red">올바르지 않은 이메일입니다.</span>
-                </div>
-              </v-col>
-            </v-card>
+            <label for="useremail" class="font-bold font-sm"> 이메일 주소</label>
+            
+            <v-text-field label="이메일을 입력해주세요" type="text" v-model="email" variant="outlined"
+              single-line hide-details
+              class="form-control font-sm mt-2" id="useremail" density="compact" :class="{
+                'is-invalid': submitted && v$.email.$error,
+              }" />
+            <div v-if="submitted && v$.email.$error" class="invalid-feedback">
+              <span v-if="v$.email.required.$invalid" class="font-xs font_red">이메일을 입력해주세요.</span>
+              <span v-if="v$.email.email.$invalid" class="font-xs font_red">올바르지 않은 이메일입니다.</span>
+            </div>
+          
             <v-row align="center" class="mt-5">
               <v-col>
-                <v-btn class="mr-5 pph-25 font-sm" @click="cancel">
+                <v-btn class="mr-5 pph-25 font-sm button_white" @click="cancel" variant="">
                   취소
                 </v-btn>
               </v-col>
               <v-col>
-                <v-btn type="submit" class="text-white pph-25 font-sm" color="rgb(var(--v-theme-primary))">
+                <v-btn type="submit" class="pph-25 font-sm button_main" variant="">
                   비밀번호 찾기
                 </v-btn>
               </v-col>
@@ -98,6 +98,26 @@ export default {
   </v-container>
 </template>
 
-<style lang="scss" module></style>
+<style lang="scss" scoped>
+.button_white {
+  border-width: 1px;
+  border-style: solid;
+  border-color: #ABABAB;
+  color: black;
+}
+
+.button_main {
+  border-width: 1px;
+  border-style: solid;
+  border-color: rgb(var(--v-theme-primary));
+  background-color: rgb(var(--v-theme-primary));
+  color: white;
+}
+
+.max-pw {
+  max-width: 500px;
+  width: 95% !important;
+}
+</style>
 
 
--- webview/src/pages/account/findpass_ok.vue
+++ webview/src/pages/account/findpass_ok.vue
@@ -14,17 +14,19 @@ export default {
 
 
 <template>
-  <v-container class="ph-100 pw-100">
+  <v-container class="ph-50 pw-100">
         <v-row class="ph-100">
           <v-col class="pw-100" align-self="center">
-            <div class="text-center">
+            <v-container>
+              <center>
               <img src="@/assets/images/check.png" width="55" height="55">
-              <h2 class="text-primary mt-20">example@gmail.com</h2>
-              <h2>으로 임시 비밀번호를 <br> 발급해드렸습니다.</h2>
-            </div>
+              </center>
+              <h2 class="text-primary text-center mt-20">example@gmail.com</h2>
+              <h3 class="text-center">으로 임시 비밀번호를 발급해드렸습니다.</h3>
+            </v-container>
             <div class="text-center">
-              <div class="col-12 mt-30">
-                <v-btn type="submit" class="text-white" color="rgb(var(--v-theme-primary))" @click="login">
+              <div class="col-12 mt-20">
+                <v-btn max-width="180" type="submit" class="text-white pw-50" color="rgb(var(--v-theme-primary))" @click="login">
                   로그인
                 </v-btn>
               </div>
