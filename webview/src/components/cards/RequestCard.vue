6d3669377ff5e05c962eee4e26d1c2f8aa5e5fa2
변상진 <gene.byun.1@gmail.com>
2023-04-18 17:09:10 +0900

    chore: RequestCard ui fix

--- webview/src/components/cards/RequestCard.vue
+++ webview/src/components/cards/RequestCard.vue
@@ -1,26 +1,26 @@
 <template>
-    <v-card class="mx-auto justify-center" :class="props?.data?.gifted ? 'gifted-card' : 'nongifted-card'"  variant="outlined" @click="handleCardClicked">
-        <v-card-item>
-        <div>
-            <v-chip class="gifted-chip mb-2 w-100" v-if="props?.data?.gifted" variant="outlined"><img src="@/assets/images/icons/party-popper.svg" class="mr-2"/>카드 드립니다!!!</v-chip>
-        </div>
-        <div>
-            <v-row>
-            <v-col cols="8" class="text-h7">{{ props?.data?.title }}</v-col>
-            <v-col cols="4" class="text-caption font-0000008F">{{ props?.data?.date }}</v-col>
-            </v-row>
-            <div class="mt-2 text-caption font-0000008F">{{ props?.data?.content }} 
+    <v-card class="mx-auto justify-center rounded-lg" :class="props?.data?.gifted ? 'gifted-card' : 'nongifted-card'"  variant="outlined" @click="handleCardClicked">
+        <v-card-item class="pb-4">
+            <div>
+                <v-chip class="gifted-chip mb-2 w-100" v-if="props?.data?.gifted" variant="outlined"><img src="@/assets/images/icons/party-popper.svg" class="mr-2"/>카드 드립니다!!!</v-chip>
             </div>
-        </div>
-        <v-chip class="mt-2" variant="outlined"><v-icon start icon="mdi-account-multiple-outline"></v-icon>{{props?.data?.members}}</v-chip>
-        <v-row class="mt-2">
-            <v-col cols="3" class="center-container align-items-end"><v-icon size="small">mdi-heart-outline</v-icon><span
-                class="text-caption ml-1">{{ props?.data?.like }}</span></v-col>
-            <v-col cols="3" class="center-container justify-content-end"><v-icon size="small">mdi-message-text-outline</v-icon><span
-                class="text-caption ml-1">{{ props?.data?.comment }}</span></v-col>
-            <v-col cols="6">
-            </v-col>
-        </v-row>
+            <div>
+                <v-row>
+                <v-col cols="8" class="text-h7">{{ props?.data?.title }}</v-col>
+                <v-col cols="4" class="text-caption font-0000008F">{{ props?.data?.date }}</v-col>
+                </v-row>
+                <div class="mt-2 mb-3 text-caption font-0000008F">{{ props?.data?.content }} 
+                </div>
+            </div>
+            <v-chip class="mt-2" variant="outlined"><v-icon start icon="mdi-account-multiple-outline"></v-icon>{{props?.data?.members}}</v-chip>
+            <v-row class="mt-2">
+                <v-col cols="3" class="center-container align-items-end"><v-icon size="small">mdi-heart-outline</v-icon><span
+                    class="text-caption ml-1">{{ props?.data?.like }}</span></v-col>
+                <v-col cols="3" class="center-container justify-content-end"><v-icon size="small">mdi-message-text-outline</v-icon><span
+                    class="text-caption ml-1">{{ props?.data?.comment }}</span></v-col>
+                <v-col cols="6">
+                </v-col>
+            </v-row>
         </v-card-item>
     </v-card>
 </template>
