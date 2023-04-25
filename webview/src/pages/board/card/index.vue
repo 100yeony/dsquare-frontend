fa71ade7e5693f315b23e1f22a8f214294942c8a
우예원 <yeweon@uyewon-ui-MacBookPro.local>
2023-04-25 17:38:08 +0900

    Fix: fix card index page

--- webview/src/pages/board/card/index.vue
+++ webview/src/pages/board/card/index.vue
@@ -1,33 +1,45 @@
 <template>
   <div class="keep-all">
+
+    <div>
+      <CardDialog :isShow="isShow" 
+      :title="dialogTitle" 
+      @click-confirm="onConfirm" 
+      @click-cancel="onCancel" />
+    </div>
+
     <!-- 카드주세요 header-->
     <p class="text-h6 font-weight-black">카드주세요</p>
     <p class="text-caption my-3 font-0000008F">
       고생하는 팀원들을 위해, 회사가 쏩니다!!!!
     </p>
 
+    <v-divider :thickness="1" class="mt-4 mb-5"></v-divider>
+
+    <!-- 금주의 카드 -->
+    <p class="mt-3 text-h6 font-weight-black">이달의 카드</p>
+    <div>
+      <RequestCard class="mt-2 card-of-the-week" :data="giftedCardData" @handle-card-clicked="handleCardClicked" @handle-card-dialog="handleCardDialog" />
+    </div>
+
+    <v-divider :thickness="1" class="mt-4 mb-5"></v-divider>
+
+    <p class="mt-3 text-h6 font-weight-black">카드 대기중</p>
     <!-- 검색 -->
     <v-expansion-panels class="my-3">
       <v-expansion-panel>
         <v-expansion-panel-title>검색</v-expansion-panel-title>
         <v-expansion-panel-text>
-          <v-row justify="center">
-            <v-col cols="6" class="mb-0 pb-0">
-              <v-select v-model="category" class="text-truncate" placeholder="분야" variant="outlined" density="compact"
-                :items="categoryItems" @update:modelValue="categoryChanged"></v-select>
+          <v-row justify="center" class="mb-2">
+            <v-col>
+              <v-select v-model="category" class="text-truncate mt-2" placeholder="부서 선택" variant="outlined"
+                density="compact" :items="categoryItems" @update:modelValue="categoryChanged" id="category"
+                hide-details></v-select>
             </v-col>
-            <v-col cols="6" class="mb-0 pb-0">
-              <v-select v-model="subcategory" class="text-truncate" placeholder="소분야" variant="outlined" density="compact"
-                :items="subcategoryItems" :disabled="!subcategoryItems.length"></v-select>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="4" class="pr-0 pt-0">
-              <v-select v-model="searchKey" placeholder="구분" class="text-truncate" variant="outlined" density="compact"
-                :items="['제목 + 내용', '작성자']"></v-select>
-            </v-col>
-            <v-col cols="8" class="pl-0 pt-0">
-              <v-text-field v-model="searchContent" placeholder="검색어" variant="outlined" density="compact" />
+            <v-col>
+              <v-select v-model="subcategory" class="text-truncate mt-2" placeholder="소속팀 선택" variant="outlined"
+                density="compact" :items="subcategoryItems" :disabled="!subcategoryItems.length" id="subcategory"
+                hide-details></v-select>
             </v-col>
           </v-row>
           <v-btn color="shades-black" @click="search()" block>검색</v-btn>
@@ -35,17 +47,10 @@
       </v-expansion-panel>
     </v-expansion-panels>
 
-    <!-- 금주의 카드 -->
-    <p class="mt-3 text-h6 font-weight-black">금주의 카드</p>
-    <div>
-      <RequestCard class="mt-2 card-of-the-week" :data="giftedCardData" @handle-card-clicked="handleCardClicked" />
-    </div>
-
     <!-- 카드 대기중 목록 -->
-    <p class="mt-3 text-h6 font-weight-black">카드 대기중</p>
     <div>
       <div v-for="(item, index) in requestCardData" :value="item.cardId" class="card">
-        <RequestCard class=" mt-2" :data="item" :key="index" @handle-card-clicked="handleCardClicked" :style="item.style"/>
+        <RequestCard class=" mt-2" :data="item" :key="index" @handle-card-clicked="handleCardClicked" @handle-card-dialog="handleCardDialog" :style="item.style"/>
       </div>
     </div>
     <Observe @triggerIntersected="loadMore" />
@@ -65,33 +70,54 @@
 <script>
 import { computed, onMounted, ref } from "vue";
 import RequestCard from "@/components/cards/RequestCard";
+import CardDialog from "@/components/cards/CardDialog.vue";
 import Observe from "@/components/Observer";
 import api from '@/api';
 import store from "@/store";
-
+import dialogUtils from "@/utils/dialogUtils"
 
 export default {
   name: "cardBoard",
   components: {
     RequestCard,
-    Observe
+    Observe,
+    CardDialog
   },
   setup() {
-    let area = store.getters["info/infoArea"];
-    let categoryItems = area.areaList;
-    let subcategoryFullList = area.subAreaList;
+    let categoryItems = ['플랫폼품질혁신TF', '플랫폼IT컨설팅vTF', '플랫폼서비스담당',
+      'Digico서비스담당', 'Digico개발센터'];
+    let subcategoryFullList = [
+      [],
+      [],
+      ["메시징DX플랫폼팀", "서비스플랫폼팀",
+        "금융결제DX플랫폼팀", "인증DX플랫폼팀"],
+      ["미디어플랫폼팀", "AI서비스팀",
+        "AICC서비스팀", "Safety플랫폼팀"],
+      ["AgileCore팀", "Digico사업수행팀", "AICC딜리버리팀"],
+    ];
 
     let searchUri = "/board/questions";
 
     return {
-      categoryItems,
-      subcategoryFullList,
+      categoryItems, subcategoryFullList,
       searchUri,
       maxStackedCards: 4,
     };
   },
   data() {
     return {
+      writerInfo: {
+        id: 0,
+        email: "",
+        nickname: "",
+        name: "",
+        contact: "",
+        teamHierarchy: []
+      },
+      projTeamInfo: {
+        tid: 0,
+        name: ""
+      },
       category: [],
       subcategory: [],
       subcategoryItems: [],
@@ -109,11 +135,56 @@ export default {
         comment: "3",
         createDate: "2023-4-21 17:46",
         selectionInfo: {},
+        projTeamInfo:{tid: 6, name: "메세징DX플랫폼팀"},
       },
+      isShow: false, 
     };
+  },
+  watch: {
+    category(newVal, oldVal) {
+      console.log(newVal)
+      if (newVal === '플랫폼품질혁신TF') {
+        this.projTeamId = 1;
+      } else if (newVal === '플랫폼IT컨설팅vTF') {
+        this.projTeamId = 2;
+      } else {
+        this.projTeamId = '';
+      }
+    },
+    subcategory(newVal, oldVal) {
+      console.log(newVal)
+      if (newVal === '메시징DX플랫폼팀') {
+        this.projTeamId = 6;
+      } else if (newVal === '서비스플랫폼팀') {
+        this.projTeamId = 7;
+      } else if (newVal === '금융결제DX플랫폼팀') {
+        this.projTeamId = 8;
+      } else if (newVal === '인증DX플랫폼팀') {
+        this.projTeamId = 9;
+      } else if (newVal === '미디어플랫폼팀') {
+        this.projTeamId = 10;
+      } else if (newVal === 'AI서비스팀') {
+        this.projTeamId = 11;
+      } else if (newVal === 'AICC서비스팀') {
+        this.projTeamId = 12;
+      } else if (newVal === 'Safety플랫폼팀') {
+        this.projTeamId = 13;
+      } else if (newVal === 'AgileCore팀') {
+        this.projTeamId = 14;
+      } else if (newVal === 'Digico사업수행팀') {
+        this.projTeamId = 15;
+      } else if (newVal === 'AICC딜리버리팀') {
+        this.projTeamId = 16;
+      }
+    }
   },
   mounted() {
     var res = this.requestAll();
+  },
+  computed: {
+    dialogTitle() {
+      return '카드를 주시겠습니까?';
+    },
   },
   methods: {
     async requestAll() {
@@ -135,6 +206,16 @@ export default {
       this.subcategoryItems = 1 <= categoryIndex ? this.subcategoryFullList[categoryIndex - 1] : [];
     },
     async search() {
+      if (typeof this.subcategory == 'string' || typeof this.category == 'string'){
+        var res = await api.get('board/cards/search?projTeamId=' + this.projTeamId).then(
+          (response) => {
+            response.data.forEach((d) => {
+              d.createDate = this.exportDateFromTimeStamp(d.createDate)
+            });
+            this.requestCardData = response.data
+          }
+        )
+      }
     },
     handleCardClicked(item) {
       if (item) {
@@ -202,7 +283,36 @@ export default {
     },
     clamp (value, min, max) {
       return Math.min(Math.max(min, value), max)
-    }
+    },
+    categoryChanged() {
+      this.subcategory = [];
+      var categoryIndex = this.categoryItems.indexOf(this.category);
+      if (categoryIndex != 0) {
+        this.subcategoryItems = this.subcategoryFullList[categoryIndex];
+      }
+      else {
+        this.subcategoryItems = [];
+      }
+    },
+    handleCardDialog() {
+      console.log('handel card dialog')
+      this.isShow = true; 
+    },
+    onConfirm() {
+      console.log('confirm payload:');
+      this.isShow = false;
+      // if (this.selectedPostType == 0) {
+      //   this.requestDelQuestion();
+      // } else if (this.selectedPostType == 1) {
+      //   this.requestDelAnswer();
+      // } else if (this.selectedPostType == 2) {
+
+      // }
+    },
+    onCancel() {
+      console.log('cancel');
+      this.isShow = false;
+    },
   },
 };
 </script>
