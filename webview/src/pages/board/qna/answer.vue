034195258098578c6334a1eb5031d9273d93d202
변상진 <gene.byun.1@gmail.com>
2023-05-12 17:28:54 +0900

    feature: qna answer attachment (unfinished)

--- webview/src/pages/board/qna/answer.vue
+++ webview/src/pages/board/qna/answer.vue
@@ -2,6 +2,7 @@
 import CKEditor from "@ckeditor/ckeditor5-vue";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import FileUploadAdapter from "@/utils/fileUploaderAdapter";
+import fileUtils from "@/utils/fileUtils";
 import { watch, onMounted, ref } from "vue";
 //import { generateKey } from "crypto";
 import api from '@/api';
@@ -20,33 +21,59 @@ export default {
   },
   data() {
     return {
+      selectedFile: null,
+      fileAttachable: false,
       editor: ClassicEditor,
       editorData: '',
       editorConfig: {
         // 상세 수정은 https://ckeditor.com
         extraPlugins: [this.uploader],
-        removePlugins: ["ImageCaption", "MediaEmbed"]
-     
+        removePlugins: ["ImageCaption", "MediaEmbed"],
       },
-
     };
   },
+  watch: {
+    selectedFile: function (newVal, oldVal) {
+      console.log(newVal)
+      if (newVal != null && newVal?.length != 0) {
+        if (fileUtils.isFileSizeLimit(newVal[0].size)) {
+          this.fileAttachable = false
+          alert('파일 크기는 10MB 이하여야 합니다.');
+          this.selectedFile = null
+        } else {
+          this.fileAttachable = true
+        }
+      } else {
+        this.fileAttachable = false
+      }
+    }
+  },
   mounted() {
-    this.area = store.getters["info/infoArea"]
-    this.$route.query.qid
+    this.area = store.getters["info/infoArea"];
   },
   methods: {
     async write(editorData) {
-      console.log(editorData);
-      await api.post('board/questions/' + this.$route.query.qid + '/answers', {
-        writerId: store.getters["info/infoUser"].userId,
-        content: this.editorData,
-        atc: {
-          "originFileName": "테스트파일",
-          "extension": "xlsx",
-          "fileSize": "512345"
+      let formData = new FormData();
+
+      const answer = {
+        content: editorData,
+      };
+
+      const answerBlob = new Blob([JSON.stringify(answer)], { type: 'application/json' });
+      formData.append('answer', answerBlob);
+      if (this.fileAttachable) {
+        formData.append("attachment", this.selectedFile[0], this.selectedFile.name);
+      }
+
+      api.multiPartPost(`board/questions/${this.$route.query.qid}/answers`, formData)
+        .then((response) => {
+          console.log(response)
+          store.dispatch('info/setPageState', {});
+        }).catch((error) => {
+          console.log(error)
         }
-      })
+      );
+
       this.cancle();
     },
     uploader(editor) {
@@ -61,7 +88,7 @@ export default {
           qid: this.$route.query.qid
         }
       });
-    }
+    },
 
   },
   computed: {
@@ -82,7 +109,7 @@ export default {
     <div class="font-sm font-medium mt-7 mb-2">답변</div>
     <ckeditor v-model="editorData" :editor="editor" :config="editorConfig" height="200"></ckeditor>
 
-    <v-file-input label="파일을 첨부해주세요." chips class="mt-5" variant="outlined" density="compact" hide-details>
+    <v-file-input v-model="selectedFile" label="파일을 첨부해주세요." chips class="mt-5" variant="outlined" density="compact" hide-details>
     </v-file-input>
 
     <v-row class="mt-5" align="center">
